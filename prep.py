0f7b59eb4128434cae15e5f9cb53b37dfba6b299
jzb <zhengbaj@cs.cmu.edu>
2021-02-02 00:31:00 -0500

    update prep

--- prep.py
+++ prep.py
@@ -2,7 +2,10 @@ from typing import Dict, List
 from collections import defaultdict
 import argparse
 import json
+import urllib
+from random import shuffle
 import numpy as np
+import matplotlib.pyplot as plot
 from dataset import Break, HoptopQA
 from rag.utils_rag import exact_match_score, f1_score
 from rag.eval_rag import get_scores
@@ -10,6 +13,7 @@ from rag.eval_rag import get_scores
 
 numhops2temps: Dict[int, List[str]] = {
   2: ['n-*', 'p-*', '*-n', '*-p', 'n-n', 'n-p', 'p-n', 'p-p']
+  #2: ['n-*', '*-n', 'n-n']
 }
 
 
@@ -19,6 +23,7 @@ def nline_to_cate(nline: int, num_hops: int):
 
 def adaptive(pred1: str, pred2: str, gold_file: str, thres: float=0.0):
   em = f1 = total = 0
+  uses = []
   with open(pred1, 'r') as p1fin, open(pred2, 'r') as p2fin, open(gold_file, 'r') as gfin:
     for line in p1fin:
       pred1, prob1 = line.rstrip('\n').split('\t')
@@ -26,19 +31,21 @@ def adaptive(pred1: str, pred2: str, gold_file: str, thres: float=0.0):
       prob1, prob2 = float(prob1), float(prob2)
       golds = gfin.readline().rstrip('\n').split('\t')
       pred = pred1 if prob1 >= thres else pred2
+      uses.append(int(prob1 >= thres))
       em += max(exact_match_score(pred, g) for g in golds)
       f1 += max(f1_score(pred, g) for g in golds)
       total += 1
-  print('em {:.2f} f1 {:.2f} total {}'.format(em / total * 100, f1 / total * 100, total))
+  print('em {:.2f} f1 {:.2f} total {}, no ret {:.2f}'.format(em / total * 100, f1 / total * 100, total, np.mean(uses) * 100))
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--task', type=str, choices=['eval', 'hotpotqa', 'ana', 'nq', 'ada'], default='hotpotqa')
+  parser.add_argument('--task', type=str, choices=['eval', 'hotpotqa', 'comqa', 'ana', 'nq', 'ada', 'same'], default='hotpotqa')
   parser.add_argument('--input', type=str, nargs='+')
   parser.add_argument('--output', type=str)
   parser.add_argument('--split', type=str, default='dev')
   parser.add_argument('--num_hops', type=int, default=2)
+  parser.add_argument('--thres', type=float, default=.0)
   parser.add_argument('--no_context', action='store_true')
   args = parser.parse_args()
 
@@ -74,7 +81,30 @@ if __name__ == '__main__':
         sfout.write('{}\n'.format(mh['q']))
         tfout.write('{}\n'.format(mh['a']))
 
+  elif args.task == 'comqa':
+    def parse_answer(answer, id):
+      if answer.startswith('http'):
+        answer = urllib.parse.unquote_plus(answer).rsplit('/', 1)[-1]
+      answer = answer.replace('\n', ' ').replace('\t', ' ')
+      return answer
+
+    with open(args.input[0], 'r') as fin, open(args.output + '.source', 'w') as sfout, open(args.output + '.target', 'w') as tfout, open(args.output + '.cluster', 'w') as cfout:
+      data = json.load(fin)
+      for ex in data:
+        answers = [parse_answer(a, ex['cluster_id']) for a in ex['answers']]
+        answers = '\t'.join(answers)
+        for q in ex['questions']:
+          cfout.write(ex['cluster_id'] + '\n')
+          sfout.write(q + '\n')
+          tfout.write(answers + '\n')
+
   elif args.task == 'ana':
+    def printify(case):
+      print('---')
+      for s, t, p in case:
+        s = s.split('\t')[0]
+        print(s, t, p)
+
     pred_file, source_file, target_file = args.input
     ems = []
     groups = []
@@ -82,17 +112,17 @@ if __name__ == '__main__':
 
     with open(pred_file, 'r') as pfin, open(source_file, 'r') as sfin, open(target_file, 'r') as tfin:
       for i, l in enumerate(pfin):
-        pred = l.strip()
+        pred = l.rstrip('\n').split('\t')[0]
         source = sfin.readline().strip()
-        target = tfin.readline().strip()
-        em = exact_match_score(pred, target)
+        targets = tfin.readline().rstrip('\n').split('\t')
+        em = max(exact_match_score(pred, target) for target in targets)
         ems.append(em)
         if i % len(numhops2temps[args.num_hops]) == 0:
           groups.append([])
           cases.append([])
         groups[-1].append(em)
-        if i % len(numhops2temps[args.num_hops]) in {0, 2, 4}:
-          cases[-1].append((source, target, pred))
+        if numhops2temps[args.num_hops][i % len(numhops2temps[args.num_hops])] in {'n-*', '*-n', 'n-n'}:
+          cases[-1].append((source, targets, pred))
     temps = numhops2temps[args.num_hops]
     groups = [dict(zip(temps, group)) for group in groups]
 
@@ -105,22 +135,23 @@ if __name__ == '__main__':
     for group, case in zip(groups, cases):
       key = '{:d}{:d}-{:d}'.format(group['n-*'], group['*-n'], group['n-n'])
       non_cate[key] += 1
-      if key == '11-0':
+      if key == '01-1':
         cases_show.append(case)
 
-      para_cate['{:d}{:d}-{:d}'.format(group['p-*'], group['*-p'], group['p-p'])] += 1
+      if 'p-*' in group:
+        para_cate['{:d}{:d}-{:d}'.format(group['p-*'], group['*-p'], group['p-p'])] += 1
 
-      if group['n-*'] and group['*-p']:
-        np_cate['np-{:d}'.format(group['n-p'])] += 1
-      if group['p-*'] and group['*-n']:
-        np_cate['pn-{:d}'.format(group['p-n'])] += 1
+        if group['n-*'] and group['*-p']:
+          np_cate['np-{:d}'.format(group['n-p'])] += 1
+        if group['p-*'] and group['*-n']:
+          np_cate['pn-{:d}'.format(group['p-n'])] += 1
 
     print(sorted(non_cate.items(), key=lambda x: x[0]))
     print(sorted(para_cate.items(), key=lambda x: x[0]))
     print(sorted(np_cate.items(), key=lambda x: x[0]))
 
     for case in cases_show[:10]:
-      print(case)
+      printify(case)
 
   elif args.task == 'nq':
     def read_file(in_fname, dir, split, ans_format='first'):
@@ -154,4 +185,45 @@ if __name__ == '__main__':
     print('train {} val {} test {}'.format(count_train, count_dev, count_test))
 
   elif args.task == 'ada':
-    adaptive(pred1=args.input[0], pred2=args.input[1], gold_file=args.input[2], thres=0.0)
+    adaptive(pred1=args.input[0], pred2=args.input[1], gold_file=args.input[2], thres=args.thres)
+
+  elif args.task == 'same':
+    pred_file, source_file, target_file, cluster_file = args.input
+    ems = []
+    cases = []
+    prev_cluster = None
+    with open(pred_file, 'r') as pfin, open(source_file, 'r') as sfin, open(target_file, 'r') as tfin, open(cluster_file, 'r') as cfin:
+      for l in pfin:
+        pred = l.rstrip('\n').split('\t')[0]
+        question = sfin.readline().rstrip('\n')
+        golds = tfin.readline().rstrip('\n').split('\t')
+        em = max(exact_match_score(pred, gold) for gold in golds)
+        cluster = cfin.readline().rstrip('\n')
+        if cluster != prev_cluster:
+          ems.append([])
+          cases.append([])
+          prev_cluster = cluster
+        ems[-1].append(em)
+        cases[-1].append((pred, golds, question))
+    cc = cw = ic = 0
+    ic_cases = []
+    for c, case in zip(ems, cases):
+      l = len(c)
+      c = sum(c)
+      if l <= 1:
+        continue
+      if c == l:
+        cc += 1
+      elif c == 0:
+        cw += 1
+      else:
+        ic += 1
+        ic_cases.append(case)
+    print('consistently correct {} wrong {}, inconsistent {}'.format(cc, cw, ic))
+    shuffle(ic_cases)
+    for case in ic_cases[:5]:
+      print('================')
+      p, g, q = case[0]
+      print(g)
+      for p, g, q in case:
+        print(q, p, sep='\t')
